c07d0fcffa2fbb28444bcfe5e184407bf4c052a4
michaelwangcode <michaelwangxv@gmail.com>
2022-09-08 16:37:46 -0400

    Display appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 
 // Array of day objects
@@ -23,10 +24,65 @@ const days = [
 ];
 
 
+// Array of appointments
+const appointmentsObject = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+
+
+
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  // Convert the appointments object into an array
+  const appointmentsArray = Object.values(appointmentsObject).map((appointment) => {
+
+    return (
+      <Appointment
+        key={appointment.id} 
+        {...appointment} 
+      />
+    )
+  })
+
 
   return (
     <main className="layout">
@@ -53,7 +109,11 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+
+        {appointmentsArray}
+
+        <Appointment key="last" time="5pm" />
+
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
 
 
 
 export default function Appointment(props) {
 
+  console.log(props.time);
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}>
+      </Header>
+
+      { props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty /> }
+
+    </article>
   )
 }
