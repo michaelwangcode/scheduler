361dbbb1ad19266b82dc985604749285b36c2da0
michaelwangcode <michaelwangxv@gmail.com>
2022-09-10 06:09:54 -0400

    Add getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 
@@ -17,9 +17,6 @@ export default function Application(props) {
     appointments: {}
   })
 
-  // Store an array of appointments for one day
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-
   // set functions
   const setDay = day => setState({...state, day});
   
@@ -28,22 +25,31 @@ export default function Application(props) {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       console.log(all);
     })
   }, [])
   
+
+  // Store an array of appointments for one day
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   // Convert the appointments object into an array
   const appointmentsArray = dailyAppointments.map((appointment) => {
+
+    const interview = getInterview(state, appointment.interview);
 
     return (
       <Appointment
         key={appointment.id} 
-        {...appointment} 
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
-    )
-  })
+    );
+  });
 
   // Return the Application component
   return (
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,7 @@
+// This function returns an array of appointments for that day
+// It takes in a state object (containing an array of days and an object of appointments)
+// It takes in a day as a String: "Monday" etc.
 export function getAppointmentsForDay(state, day) {
-  //... returns an array of appointments for that day
-
-  // Takes in a state object (containing an array of days and an object of appointments)
-
 
   // Store the appointment days by iterating through the days array
   let days = state["days"];
@@ -23,3 +22,31 @@ export function getAppointmentsForDay(state, day) {
 
   return appointmentDetails;
 }
+
+
+
+// This function returns an object contaning interview data 
+// It takes an object that contains the interviewer
+export function getInterview(state, interview) {
+
+  // Store the interviewers portion of the state object
+  let interviewers = state.interviewers;
+
+  // If either input is invalid, return null
+  if (!interviewers || !interview) {
+    return null;
+  }
+
+  // Store the interviewer data object 
+  const interviewerData = interviewers[interview.interviewer];
+
+  // Return an onject containing the student name and interviewer data object
+  let result = {
+    student: interview.student,
+    interviewer: interviewerData
+  }
+
+  return result;
+}
+
+
