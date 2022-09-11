d9c266c0c0cc8ed9d8655cb7adcdbe7a867f452e
michaelwangcode <michaelwangxv@gmail.com>
2022-09-11 03:13:32 -0400

    Add tests for hooks

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,68 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useVisualMode from "hooks/useVisualMode";
+
+
+// Name of the modes
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
+
+
+// Initialize the mode to FIRST and return it
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+// Initialize the mode to FIRST, then transition to SECOND and return it
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
+
+// Initialize the mode to FIRST
+// Transition from FIRST to SECOND to THIRD, then back to SECOND and FIRST
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+// Back function should not work if history array only has one value
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+
+// Transition twice and have the back action return to the initial state
+// Replace the current mode in history with a new one
+// This is done by passing "true" as an optional argument
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
