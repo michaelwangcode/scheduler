528f89f862c52f306b5761520be6149bb50288b0
michaelwangcode <michaelwangxv@gmail.com>
2022-09-15 02:36:53 -0400

    Add Application tests

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,94 @@
+// This file is used to mock the axios library for testing
+// The Jest framework will replace any axios import with this mock module instead
+
+
+// Database used for testing
+const fixtures = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2],
+      interviewers: [1, 2],
+      spots: 1
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [3, 4],
+      interviewers: [3, 4],
+      spots: 1
+    }
+  ],
+  appointments: {
+    "1": { id: 1, time: "12pm", interview: null },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: { student: "Archie Cohen", interviewer: 2 }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Leopold Silvers", interviewer: 4 }
+    },
+    "4": { id: 4, time: "3pm", interview: null }
+  },
+  interviewers: {
+    "1": {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    },
+    "3": {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png"
+    },
+    "4": {
+      id: 4,
+      name: "Cohana Roy",
+      avatar: "https://i.imgur.com/FK8V841.jpg"
+    }
+  }
+};
+
+
+
+// Export an object with a get property
+export default {
+
+  // Use jest.fn to create the mock data
+  get: jest.fn(url => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "/api/appointments") {
+      /* Resolve appointments data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+
+    if (url === "/api/interviewers") {
+      /* Resolve interviewers data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  })
+}
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,21 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+
+// Check to see if the page loads on Monday
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  // Wait until we can get a DOM element with the text "Monday"
+  return waitForElement(() => getByText("Monday")).then(() => {
+
+    // Click on "Tuesday" 
+    fireEvent.click(getByText("Tuesday"));
+
+    // Expect to see the student named "Leopold Silvers"
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
